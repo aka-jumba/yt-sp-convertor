7c7d60712debcc603e2c869db49070098b8d7959
Aayush Joglekar <aayushjog@gmail.com>
2021-06-25 21:34:03 +0530

    Add Sp2Yt component

--- frontend/src/SpotifyToYoutube/index.js
+++ frontend/src/SpotifyToYoutube/index.js
@@ -1,40 +1,93 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import Header from "../Header";
+import { Form, Button } from "react-bootstrap";
+import DownArrow from "../assets/downloading_white_48dp.svg";
+
+// const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 
 export default (props) => {
+  const [url, setUrl] = useState(
+    "https://open.spotify.com/playlist/1Wr1URyL0fZcW10cHyxgpj"
+  );
+  const [name, setName] = useState("New Playlist");
+
+  const onConvert = async (e) => {
+    e.preventDefault();
+    console.log(url, name);
+  };
+
   return (
     <>
       <Header />
       <div className="sp-to-yt">
-        <div className="bubble p-4 d-flex justify-content-start">
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
-                Spotify Link
-              </label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
+        <div
+          className="d-flex flex-column align-items-center"
+          style={{ padding: "3rem 4rem", backgroundColor: "#1DB954" }}
+        >
+          <Form
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Form.Group controlId="formBasicEmail" style={{ width: "75%" }}>
+                <Form.Label style={{ fontSize: "1.5rem", fontWeight: "600" }}>
+                  Spotify Playlist Link
+                </Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  placeholder="Eg: https://open.spotify.com/playlist/1Wr1URyL0fZcW10cHyxgpj"
+                />
+              </Form.Group>
+              <Form.Group>
+                <div
+                  style={{
+                    margin: "3rem 4rem 2rem 4rem",
+                    transform: "rotate(-90deg)",
+                  }}
+                >
+                  <img src={DownArrow} height="75" alt="Down Arrow" />
+                </div>
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail" style={{ width: "75%" }}>
+                <Form.Label style={{ fontSize: "1.5rem", fontWeight: "600" }}>
+                  Youtube Playlist Name
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Eg: New Playlist"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
-                Youtube Playlist name
-              </label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-              />
+            <div>
+              <Button
+                className="mt-1"
+                variant="light"
+                type="submit"
+                onClick={onConvert}
+              >
+                Convert
+              </Button>
             </div>
-            <button type="submit" class="btn btn-primary">
-              Convert
-            </button>
-          </form>
+          </Form>
         </div>
       </div>
     </>
--- frontend/src/YoutubeToSpotify/index.js
+++ frontend/src/YoutubeToSpotify/index.js
@@ -10,28 +10,28 @@ export default (props) => {
       <div className="yt-to-sp">
         <div className="bubble p-4 d-flex justify-content-start">
           <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
                 Youtube Link
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
               />
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
                 Spotify Playlist name
               </label>
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="exampleInputPassword1"
               />
             </div>
-            <button type="submit" class="btn btn-primary">
+            <button type="submit" className="btn btn-primary">
               Convert
             </button>
           </form>
