1bcceecf431905daea0e1b2a117b4c648fe7df64
Aayush Joglekar <aayushjog@gmail.com>
2021-06-25 19:51:38 +0530

    Refactor to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import SpotifyToYoutube from "./SpotifyToYoutube/";
+import YoutubeToSpotify from "./YoutubeToSpotify/";
 
 function App() {
   return (
@@ -8,65 +10,11 @@ function App() {
           <h1>Spotify Youtube App</h1>
         </div>
       </div>
-      <div className="yt-to-sp mt-4">
-        <div className="bubble p-4 d-flex justify-content-start">
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
-                Youtube Link
-              </label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
-                Spotify Playlist name
-              </label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-            <button type="submit" class="btn btn-primary">
-              Convert
-            </button>
-          </form>
-        </div>
+      <div className="mt-4">
+        <YoutubeToSpotify />
       </div>
-      <div className="sp-to-yt">
-        <div className="bubble p-4 d-flex justify-content-start">
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
-                Spotify Link
-              </label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
-                Youtube Playlist name
-              </label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-            <button type="submit" class="btn btn-primary">
-              Convert
-            </button>
-          </form>
-        </div>
+      <div className="mt-4">
+        <SpotifyToYoutube />
       </div>
     </div>
   );
---
+++ frontend/src/SpotifyToYoutube/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+export default (props) => {
+  return (
+    <div className="sp-to-yt">
+      <div className="bubble p-4 d-flex justify-content-start">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Spotify Link
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              Youtube Playlist name
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Convert
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
